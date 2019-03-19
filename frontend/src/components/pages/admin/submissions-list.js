39516141c5c04889b19559ab899ae9affe7dbdf4
moshfiqrony <moshfiqrony@gmail.com>
2019-03-20 00:18:09 +0600

    submission list fixed

--- frontend/src/components/pages/admin/submissions-list.js
+++ frontend/src/components/pages/admin/submissions-list.js
@@ -4,7 +4,10 @@ import SiderMain from '../../sidebar/sidebar-admin';
 import HeaderMain from '../../header/header-main';
 import axios from 'axios';
 
-import {Button, Layout, Table,} from "antd";
+import {Form, Layout, Select, Table} from "antd";
+
+const {Option} = Select;
+
 
 class Submissions extends React.Component {
     constructor() {
@@ -16,14 +19,34 @@ class Submissions extends React.Component {
             campCnt: 10,
             agentCnt: 20,
             submissions: [],
+            dists: [],
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         axios.get(`http://127.0.0.1:8000/api/locationView/?campgDetails__campaignId__id=${this.props.match.params.campaignId}`)
             .then(res => this.setState({
                 submissions: res.data,
-            }))
+            }));
+        axios.get('http://127.0.0.1:8000/api/districts/')
+            .then(res => this.setState({
+                dists: res.data,
+            }));
+    }
+
+    handleChange(district) {
+        if(district !== 'all'){
+            axios.get(`http://127.0.0.1:8000/api/locationView/?campgDetails__campaignId__id=${this.props.match.params.campaignId}&&campgDetails__agentId__district=${district}`)
+            .then(res => this.setState({
+                submissions: res.data,
+            }));
+        }else{
+            axios.get(`http://127.0.0.1:8000/api/locationView/?campgDetails__campaignId__id=${this.props.match.params.campaignId}`)
+            .then(res => this.setState({
+                submissions: res.data,
+            }));
+        }
     }
 
     render() {
@@ -37,25 +60,25 @@ class Submissions extends React.Component {
             dataIndex: 'date',
             key: 'date',
             width: '10%',
-        },{
-            title: 'Name',
+        }, {
+            title: 'Cl Name',
             dataIndex: 'campgDetails.clId.name',
-            key: 'name',
-            width: '15%',
-        },{
-            title: 'Name',
-            dataIndex: 'campgDetails.clId.phone',
-            key: 'name',
+            key: 'clname',
             width: '15%',
         }, {
-            title: 'Name',
-            dataIndex: 'campgDetails.agentId.name',
-            key: 'name',
+            title: 'CL Phone',
+            dataIndex: 'campgDetails.clId.phone',
+            key: 'clphone',
             width: '15%',
-        },{
-            title: 'Name',
+        }, {
+            title: 'Agent Name',
+            dataIndex: 'campgDetails.agentId.name',
+            key: 'agname',
+            width: '15%',
+        }, {
+            title: 'Agent Phone',
             dataIndex: 'campgDetails.agentId.phone',
-            key: 'name',
+            key: 'agphone',
             width: '10%',
         }, {
             title: 'Location',
@@ -68,13 +91,6 @@ class Submissions extends React.Component {
             key: 'dataAmount',
             width: '5%',
             render: (text, record) => record.amount == 0 ? null : record.amount
-        }, {
-            title: 'Action',
-            key: 'action',
-            className: 'hideforpdf',
-            width: '5%',
-            render: (text, record) => <Button onClick={() => this.handleRemove(record.id)}
-                                              type='danger'>Reset</Button>
         }
         ];
         return (
@@ -83,8 +99,21 @@ class Submissions extends React.Component {
                     <SiderMain/>
                     <Layout style={{marginLeft: 200}}>
                         <HeaderMain data={this.state.title}/>
-                        <div style={{margin: '24px 16px 0', overflow: 'initial', backgroundColor:'#fff', padding: 10}}>
-                            <Table rowKey='id' pagination={false} columns={columns} dataSource={this.state.submissions}/>
+                        <div style={{margin: '24px 16px 0', overflow: 'initial', backgroundColor: '#fff', padding: 10}}>
+                            <div>
+                                <Form style={{width: 300}}>
+                                    <Form.Item>
+                                        <Select placeholder='Select District' onChange={this.handleChange}>
+                                            <Option key={'all'} value={'all'}>All</Option>
+                                            {this.state.dists.map(dist => {
+                                                return(<Option key={dist.id} value={dist.id}>{dist.name}</Option>)
+                                            })}
+                                        </Select>
+                                    </Form.Item>
+                                </Form>
+                                <Table rowKey='id' pagination={false} columns={columns}
+                                       dataSource={this.state.submissions}/>
+                            </div>
                         </div>
                         <FooterMain/>
                     </Layout>
