503ab37acd4a69ba8eff69ed1e21d4baecf125b8
Md. Moshfiqur Rahman Rony <moshfiqrony@gmail.com>
2019-04-01 15:42:03 +0600

    MR | main reducers for login

---
+++ frontend/src/reducers/load-user.js
@@ -0,0 +1,20 @@
+let inistate = {
+    isLogin: false,
+};
+
+
+export default function (state = inistate, action) {
+
+    switch (action.type) {
+        case 'load_users':
+            return action.payload;
+        default:
+            let data = localStorage.getItem('users');
+            console.log(data);
+            if (data === null) {
+                return state;
+            } else {
+                return data;
+            }
+    }
+}
