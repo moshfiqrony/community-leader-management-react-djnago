a4cd25927d56742b7dd3c676086b2c07196c2ca4
Md. Moshfiqur Rahman Rony <moshfiqrony@gmail.com>
2019-04-02 13:29:00 +0600

    MR | agent dashboard info reduced

--- frontend/src/components/pages/agents/container/dashboard.js
+++ frontend/src/components/pages/agents/container/dashboard.js
@@ -12,14 +12,14 @@ export default function (props) {
             <div style={{padding: 24, background: '#fff', textAlign: 'center', minHeight: '79vh'}} className='center'>
                 <h5>Welcome {props.data.name} to your dashboard</h5>
                 <h6>Today is {new Date().toDateString()}</h6>
-                <div style={{width: 700, margin: '0 auto', height: 'auto', marginTop: 40}} className='center'>
-                    <div className='CountBadge center  pink darken-2'>
-                        <h5 className='textColor'>Total Campaign</h5>
-                    </div>
-                    <div className='CountBadge center  purple'>
-                        <h5 className='textColor'>Total Agents</h5>
-                    </div>
-                </div>
+                {/*<div style={{width: 700, margin: '0 auto', height: 'auto', marginTop: 40}} className='center'>*/}
+                {/*    <div className='CountBadge center  pink darken-2'>*/}
+                {/*        <h5 className='textColor'>Total Campaign</h5>*/}
+                {/*    </div>*/}
+                {/*    <div className='CountBadge center  purple'>*/}
+                {/*        <h5 className='textColor'>Total Agents</h5>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </Content>
     )
