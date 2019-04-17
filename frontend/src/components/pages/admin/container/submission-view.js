f3d1a3995e47e03d7a7cd3eb0f2a21971ee99144
moshfiqrony <moshfiqrony@gmail.com>
2019-04-18 00:18:40 +0600

    MR | SCLMS V.0005 | data submission view updated

--- frontend/src/components/pages/admin/container/submission-view.js
+++ frontend/src/components/pages/admin/container/submission-view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Drawer, Icon, Input, Table} from 'antd';
+import {Button, Drawer, Icon, Input, Table, Card} from 'antd';
 import Highlighter from 'react-highlight-words';
 import CampaignDataDetailView from "./CampaignDataDetailView"
 import data from '../../../../reducers/pabna-survey-data';
@@ -25,6 +25,53 @@ class CampaignDataTabularView extends React.Component {
 
     componentDidUpdate(prevProps, prevState, snapshot) {
 
+    }
+
+    hrs24() {
+        let cnt = 0;
+        this.state.agents.map(record => {
+            if (((new Date()) - (new Date(record._submission_time + '+0000'))) / 1000 < 3600 * 24) {
+                cnt++;
+            }
+        });
+        return cnt;
+        console.log('I am called24');
+    }
+
+    avgSubmission() {
+        let tmp1 = 0;
+        this.state.agents.map(record => {
+            let dur = ((new Date(record.end + '00').getTime() - new Date(record.start + '00').getTime()));
+            tmp1 += dur;
+        });
+
+        {
+        let dur;
+        let tmp;
+        let ar;
+        let d, h, m, s;
+        tmp1 = tmp1/this.state.agents.length;
+        if (tmp1 / 1000 / 60 > 60) {
+            // we got minute
+            if (tmp1 / 1000 / 60 / 60 > 24) {
+                d = parseInt((tmp1/1000/60/60/24).toString())
+                h = parseInt(((tmp1/1000/60/60)%24).toString());
+                m = parseInt(((tmp1/1000/60)%60).toString());
+                s = parseInt(((tmp1/1000)%60).toString());
+                dur = d + 'd ' + h + 'h ' + m + 'm '+ s + 's ';
+            } else {
+                h = parseInt((tmp1/1000/60/60).toString());
+                m = parseInt(((tmp1/1000/60)%60).toString());
+                s = parseInt(((tmp1/1000)%60).toString());
+                dur = h + 'h ' + m + 'm '+ s + 's ';
+            }
+        } else {
+                m = parseInt(((tmp1/1000/60)%60).toString());
+                s = parseInt(((tmp1/1000)%60).toString());
+                dur = m + 'm '+ s + 's ';
+        }
+        return dur;
+    }
     }
 
     getColumnSearchProps = (dataIndex) => ({
@@ -103,6 +150,33 @@ class CampaignDataTabularView extends React.Component {
         });
     };
 
+    handleDuration(time) {
+        let dur;
+        let tmp;
+        let ar;
+        let d, h, m, s;
+        if (time / 1000 / 60 > 60) {
+            // we got minute
+            if (time / 1000 / 60 / 60 > 24) {
+                d = parseInt((time/1000/60/60/24).toString())
+                h = parseInt(((time/1000/60/60)%24).toString());
+                m = parseInt(((time/1000/60)%60).toString());
+                s = parseInt(((time/1000)%60).toString());
+                dur = d + 'd ' + h + 'h ' + m + 'm '+ s + 's ';
+            } else {
+                h = parseInt((time/1000/60/60).toString());
+                m = parseInt(((time/1000/60)%60).toString());
+                s = parseInt(((time/1000)%60).toString());
+                dur = h + 'h ' + m + 'm '+ s + 's ';
+            }
+        } else {
+                m = parseInt(((time/1000/60)%60).toString());
+                s = parseInt(((time/1000)%60).toString());
+                dur = m + 'm '+ s + 's ';
+        }
+        return dur;
+    }
+
 
     render() {
         const columns = [
@@ -161,8 +235,7 @@ class CampaignDataTabularView extends React.Component {
                 title: 'Duration',
                 key: 'duration',
                 width: '8%',
-                render: (text, record) =>
-                    <p>{((new Date(record.end + '00').getTime() - new Date(record.start + '00').getTime()) / 1000 / 60).toFixed(2)} Min</p>,
+                render: (text, record) => this.handleDuration(((new Date(record.end + '00').getTime() - new Date(record.start + '00').getTime()))),
             },
             {
                 title: 'Submission Time',
@@ -188,9 +261,104 @@ class CampaignDataTabularView extends React.Component {
 
         return (
             <div>
-                <div>
-                    <h4>Total Submissions : {this.state.agents.length}</h4>
-                </div>
+                <div style={{padding: '0px 10px 0px 10px', overflow: 'auto'}}>
+                            <div style={{width: '1200', margin: '0 auto', overflow: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                                <div style={{float: 'left', width: 300, margin: 10}}>
+                                    <Card
+                                        hoverable
+                                        title={'Total Submissions'}
+                                        headStyle={{
+                                            backgroundColor: '#f7f7f7',
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5,
+                                            textAlign: 'center'
+                                        }}
+                                        style={{
+                                            backgroundColor: '#f7f7f7',
+                                            width: 300,
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5, textAlign: 'center',
+                                        }}
+                                    >
+                                        <h1 style={{
+                                            fontSize: 50,
+                                            margin: 0,
+                                            marginTop: -24
+                                        }}>{this.state.agents.length}</h1>
+                                    </Card>
+                                </div>
+                                <div style={{float: 'left', width: 'auto', margin: 10}}>
+                                    <Card
+                                        hoverable
+                                        title={'Average Duration of Submissions'}
+                                        headStyle={{
+                                            backgroundColor: '#f7f7f7',
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5,
+                                            textAlign: 'center'
+                                        }}
+                                        style={{
+                                            backgroundColor: '#f7f7f7',
+                                            width: 'auto',
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5,
+                                            textAlign: 'center'
+                                        }}
+                                    >
+                                        <h1 style={{
+                                            fontSize: 50,
+                                            margin: 0,
+                                            marginTop: -24
+                                        }}>{this.avgSubmission()}</h1>
+                                    </Card>
+                                </div>
+                                <div style={{float: 'left', width: 300, margin: 10}}>
+                                    <Card
+                                        hoverable
+                                        title={'Submissions in Last 24 Hours'}
+                                        headStyle={{
+                                            backgroundColor: '#f7f7f7',
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5,
+                                            textAlign: 'center'
+                                        }}
+                                        style={{
+                                            backgroundColor: '#f7f7f7',
+                                            width: 300,
+                                            color: '#3F4654',
+                                            border: '1px',
+                                            borderRadius: 5,
+                                            textAlign: 'center',
+                                        }}
+                                    >
+                                        <h1 style={{fontSize: 50, margin: 0, marginTop: -24}}>{this.hrs24()}</h1>
+                                    </Card>
+                                </div>
+                            </div>
+
+                            {/*<h3>Total Submissions : {this.state.agents.length}</h3>*/}
+                            {/*<h3>Average Duration of Survey : {this.avgSubmission()}</h3>*/}
+                            {/*<h3>Submissions in Last 24 Hours: {this.hrs24()}</h3>*/}
+                            {/*<div style={{float: 'right', marginTop: 115}}>*/}
+                            {/*{this.state.UpdatedData.length !== 0 ?*/}
+                            {/*<CSVLink data={this.state.UpdatedData}><p style={{backgroundColor: '#40a9ff', color: '#fff',width: 107,display: 'block', border: '1px solid', borderRadius: 5, padding: 7, float: 'right', marginTop: '-2px'}}>Download CSV</p></CSVLink> : null}*/}
+                            {/*</div>*/}
+                        </div>
+                        {/*eslint-disable*/}
+                        <div style={{display: 'flex',justifyContent: 'flex-end', margin: 10}}>
+                            <Button
+                            htmlType='button'
+                            type='primary'
+                            onClick={this.handleLoad}
+                        >
+                            <Icon type="sync" spin={this.state.visibleLoading} />Refresh
+                        </Button>
+                        </div>
                 <Table
                     rowKey={'meta/instanceID'}
                     style={{cursor: 'pointer'}}
